3c18dda1c67e02eacb2d880f0d3494179d292aaf
abikonya <36229338+abikonya@users.noreply.github.com>
2019-05-03 19:35:32 +0700

    add project files

---
+++ test_site/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import success
+
+from django.views.generic import TemplateView
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="auth.html")),
+    url(r'^success/', success),
+]
